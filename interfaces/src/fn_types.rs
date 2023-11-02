c8624a24a308149ac7872c0caddb49d67e841b95
Yakiyo <yakiyo1206@gmail.com>
2023-11-02 10:48:59 +0000

    feat: fn types

--- interfaces/src/fn_types.rs
+++ interfaces/src/fn_types.rs
@@ -1,5 +1,9 @@
-use crate::{structs::SearchResult, Chapter};
+use crate::{Chapter, SearchResult, Series};
 use extism_pdk::FnResult;
+
+/// Get metadata function signature
+/// Function name: `metadata`
+pub type GetMetadataFn = dyn Fn() -> FnResult<()>;
 
 /// Search function signature
 /// Function name: `search`
@@ -8,10 +12,20 @@ use extism_pdk::FnResult;
 /// The search term should be provided as argument
 pub type SearchFn = dyn Fn(String) -> FnResult<Vec<SearchResult>>;
 
+/// Get Series function signature
+/// Function name: `get_series`
+///
+/// Returns info related to a series. The argument passed is the series ID
+pub type GetSeriesFn = dyn Fn(String) -> FnResult<Series>;
+
 /// Get chapter function signature
 /// Function name: `get_chapters`
 ///
-/// Return all chapters of a series
+/// Return all chapters of a series. The argument passed is the series ID
 pub type GetChaptersFn = dyn Fn(String) -> FnResult<Vec<Chapter>>;
 
-pub type GetInfoFn = dyn Fn(String) -> FnResult<()>;
+/// Get images function signature
+/// Function name: `get_images`
+///
+/// Returns a vec of urls of all the images for a chapter. The argument passed is the chapter entry
+pub type GetImagesFn = dyn Fn(Chapter) -> FnResult<Vec<String>>;
