d70cda7a91d7f2f3decd24cebf3ad096523de022
Yakiyo <yakiyo1206@gmail.com>
2023-11-03 12:43:40 +0000

    chore: smoll stuff

--- plugins/mangasee/src/lib.rs
+++ plugins/mangasee/src/lib.rs
@@ -1,8 +1,20 @@
 use extism_pdk::*;
+use headless::Browser;
+use interfaces::{metadata, Metadata, SearchResult};
+
+const BASE_URL: &'static str = "https://mangasee123.com";
 
 #[plugin_fn]
-pub fn search(_: ()) -> FnResult<()> {
-    Ok(())
+pub fn metadata(_: ()) -> FnResult<Metadata> {
+    Ok(metadata!(BASE_URL))
+}
+
+#[plugin_fn]
+pub fn search(query: String) -> FnResult<Json<Vec<SearchResult>>> {
+    let browser = Browser::default()?;
+    let page = browser.new_tab()?;
+    page.navigate_to(format!("{BASE_URL}/search/?name={query}").as_str())?.wait_until_navigated()?;
+    Ok(Json(vec![]))
 }
 
 #[cfg(test)]
