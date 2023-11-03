cb858dc2969492a48e8b7cdeab4ad625a425a5c1
Yakiyo <yakiyo1206@gmail.com>
2023-11-03 13:27:37 +0000

    feat: try something

--- plugins/mangasee/src/lib.rs
+++ plugins/mangasee/src/lib.rs
@@ -1,6 +1,8 @@
 use extism_pdk::*;
 use headless::Browser;
 use interfaces::{metadata, Metadata, SearchResult};
+use scraper::{Html, Selector};
+use url::Url;
 
 const BASE_URL: &'static str = "https://mangasee123.com";
 
@@ -9,13 +11,32 @@ pub fn metadata(_: ()) -> FnResult<Metadata> {
     Ok(metadata!(BASE_URL))
 }
 
-#[plugin_fn]
-pub fn search(query: String) -> FnResult<Json<Vec<SearchResult>>> {
+// #[plugin_fn]
+pub fn search(query: &str) -> FnResult<Json<Vec<SearchResult>>> {
     let browser = Browser::default()?;
     let page = browser.new_tab()?;
-    page.navigate_to(format!("{BASE_URL}/search/?name={query}").as_str())?.wait_until_navigated()?;
+    page.navigate_to(
+        Url::parse_with_params(format!("{BASE_URL}/search/").as_str(), [("name", query)])?
+            .as_str(),
+    )?
+    .wait_until_navigated()?;
+
+    let doc = Html::parse_document(&page.get_content()?);
+    let res: Vec<_> = doc.select(&Selector::parse(".top-15.ng-scope").unwrap()).map(|s| {
+        let first = s.select(&Selector::parse(r#".SeriesName[ng-bind-html="Series.s"]"#).unwrap()).next().unwrap();
+        // (first.text(), first.attr("href"))
+        first.html()
+    }).collect();
+    println!("{:?}", res);
     Ok(Json(vec![]))
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use super::*;
+
+    #[test]
+    fn search_test() {
+        search("chainsaw").unwrap();
+    }
+}
